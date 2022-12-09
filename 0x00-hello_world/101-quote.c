983313417efd98a5d720e484051b4dd4a397bec3
root <uchepaul26>
2022-12-09 06:44:24 -0800

    fix: betty lint

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,11 +6,12 @@
  * Description: print to stdout using write funtion
  *
  * Return: 0 (error)
- */
+*/
 
 int main(void)
 {
-	char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,";
-	write(1, text, 59);
+	char tx[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
+
+	write(1, tx, 59);
 	return (1);
 }
