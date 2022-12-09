8aaa6cd74f485d83a039376292249ca90ac06d94
root <uchepaul26>
2022-12-09 06:48:07 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	char tx[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
+	char tx[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, tx, 59);
 	return (1);
