a98dd4bd2b293cf589219210f554a92b1166311d
root <uchepaul26>
2022-12-13 17:51:03 -0800

    7 last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,12 @@
 
 int print_last_digit(int n)
 {
-	n = n % 10;
-	return (n);
+	int last;
+
+	if (n < 0)
+		last = -1 * (n % 10);
+	else
+		last = n % 10;
+	_putchar(last + '0');
+	return (last);
 }
