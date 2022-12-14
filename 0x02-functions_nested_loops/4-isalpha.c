9413885d79bdbc6d160b12e2b95729f45cc17659
root <uchepaul26>
2022-12-13 18:36:06 -0800

    4 is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,12 +5,17 @@
  * @c: input parameter of the function
  * Return: 1 if successful
  *	0 otherwise
- */
+*/
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 &&
-	    c >= 65 && c >= 90)
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
 		return (1);
-	return (0);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
 }
