22573226806996af6e7bf255e4c6eef29ff2a39a
root <uchepaul26>
2022-12-22 19:47:08 -0800

    tasks 3-9

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ * @str: string to print
+ */
+
+void _puts(char *s)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+}
